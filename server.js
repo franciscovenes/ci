7964e1a77c729c711347f86b6f414a6311005d01
francisco.venes <francisco.venes@gmail.com>
2026-06-14 17:58:26 +0100

    add second image

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 
 const port = 3000;
 app.listen(port, '0.0.0.0', () => {
+  // small comment
   // eslint-disable-next-line no-console
   console.log(`Server listening on port ${port}`);
 });
